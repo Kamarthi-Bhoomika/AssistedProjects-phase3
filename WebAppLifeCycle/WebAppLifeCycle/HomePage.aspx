f9af21aea230884743608a113b5d014bc3a99056
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-08-25 16:11:36 +0530

    Section 5

---
+++ WebAppLifeCycle/WebAppLifeCycle/HomePage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="WebAppLifeCycle.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="LblMsg" runat="server"></asp:Label>
+            <br />
+            <asp:Button ID="BtnSubmit" runat="server" OnClick="BtnSubmit_Click" Text="Submit" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebAppLifeCycle/WebAppLifeCycle/HomePage.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppLifeCycle
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            LblMsg.Text += "<br>Pre Init Text";
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                LblMsg.Text += "<br>Page Load and page is not postback yet first time";
+            }
+        }
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            LblMsg.Text += "<br>Page PreRender Text";
+        }
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            LblMsg.Text += "<br>I am Button click Event";
+        }
+    }
+}
