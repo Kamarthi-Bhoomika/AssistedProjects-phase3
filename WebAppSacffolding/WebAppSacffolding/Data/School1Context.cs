0c59d96ccf39b6d5f5d680880cea596c3e546c78
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-09-04 19:39:36 +0530

    Section 4

---
+++ WebAppSacffolding/WebAppSacffolding/Data/School1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppSacffolding.Models;
+
+namespace WebAppSacffolding.Data
+{
+    public class School1Context : DbContext
+    {
+        public School1Context (DbContextOptions<School1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppSacffolding.Models.Student> Student { get; set; } = default!;
+    }
+}
